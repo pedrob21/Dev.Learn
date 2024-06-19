3dc54ba14e538f4f82093a642841ac387cbcd96c
pedrob21 <154927859+pedrob21@users.noreply.github.com>
2024-06-19 17:13:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <title>USPFLIX</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <a href="https://www.icmc.usp.br/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <img src="https://avatars.githubusercontent.com/u/31321925?s=200&v=4" width="50px">
+          </a>
+        </header>
+      </div>
+
+   
+    </header>
+  </div>
+
+    <div class="px-4 py-5 my-5 text-center">
+        <h1 class="display-5 fw-bold text-body-emphasis">USPFLIX</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">Encontre seu filme favorito!</p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <input type="text" id="nomeFilme" placeholder="Nome do filme">
+            <button type="button" class="btn btn-outline-secondary btn-lg px-4">Pesquisar</button>
+          </div>
+        </div>
+      </div>
+</body>
+
+</html>
