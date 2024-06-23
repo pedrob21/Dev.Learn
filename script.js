b306d5c3bf6dc84590f7dd317d5a7f09c1be0f85
larissa-dma <larissadma@usp.br>
2024-06-23 12:12:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,47 +29,49 @@
 
   <div class="filmes"></div>
 
-     async function Pesquisar() {
-      const inputValor = document.getElementById('pesquisar').value.trim();
-      if (inputValor === '') {
-        alert('Digite o nome do filme ou série para pesquisar.');
-        return;
-      }
+   async function Pesquisar() {
+  const inputValor = document.getElementById('pesquisar').value.trim();
+  if (inputValor === '') {
+    alert('Digite o nome do filme ou série para pesquisar.');
+    return;
+  }
 
-      const url = `https://imdb-top-100-movies.p.rapidapi.com/search?type=title&term=${encodeURIComponent(inputValor)}`;
+  const url = `https://imdb-top-100-movies.p.rapidapi.com/search?type=title&term=${encodeURIComponent(inputValor)}`;
 
-      const xhr = new XMLHttpRequest();
-      xhr.withCredentials = true;
+  const xhr = new XMLHttpRequest();
+  xhr.withCredentials = true;
 
-      xhr.addEventListener('readystatechange', function () {
-        if (this.readyState === this.DONE) {
-          if (this.status === 200) {
-            const response = JSON.parse(this.responseText);
-            console.log(response);
-            const filmesDiv = document.querySelector('.filmes');
-            filmesDiv.innerHTML = '';
-            if (response.results && response.results.length > 0) {
-              response.results.forEach(filme => {
-                const titulo = filme.title;
-                const elementoFilme = document.createElement('p');
-                elementoFilme.textContent = titulo;
-                filmesDiv.appendChild(elementoFilme);
-              });
-            } else {
-              filmesDiv.textContent = 'Nenhum resultado foi encontrado.';
-            }
-          } else {
-            console.error('Erro na requisição:', this.status);
-            alert('Ocorreu um erro na requisição. Por favor, tente novamente mais tarde.');
-          }
+  xhr.addEventListener('readystatechange', function () {
+    if (this.readyState === this.DONE) {
+      if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
+        const filmesDiv = document.querySelector('.filmes');
+        filmesDiv.innerHTML = '';
+        if (response.results && response.results.length > 0) {
+          response.results.forEach(filme => {
+            const titulo = filme.title;
+            const elementoFilme = document.createElement('p');
+            elementoFilme.textContent = titulo;
+            filmesDiv.appendChild(elementoFilme);
+          });
+        } else {
+          filmesDiv.textContent = 'Nenhum resultado foi encontrado.';
         }
-      });
+      } else {
+        console.error('Erro na requisição:', this.status);
+        alert('Ocorreu um erro na requisição. Por favor, tente novamente mais tarde.');
+      }
+    }
+  });
 
-      xhr.open('GET', 'https://imdb-top-100-movies.p.rapidapi.com/');
-      xhr.setRequestHeader('x-rapidapi-key', 'b9715b912amsh8c346f5f7c302b3p1a80d4jsn47b14aaea364');
-      xhr.setRequestHeader('x-rapidapi-host', 'imdb-top-100-movies.p.rapidapi.com');
+  xhr.open('GET', url);
+  xhr.setRequestHeader('x-rapidapi-key', 'b9715b912amsh8c346f5f7c302b3p1a80d4jsn47b14aaea364');
+  xhr.setRequestHeader('x-rapidapi-host', 'imdb-top-100-movies.p.rapidapi.com');
 
-      xhr.send(data);
+  xhr.send();
+}
+
 </body>
 
 </html>
