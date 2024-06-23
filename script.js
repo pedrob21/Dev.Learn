45865a3f134e6fd876126ddbe5b96d07b8d6cad3
larissa-dma <larissadma@usp.br>
2024-06-23 12:38:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <title>USPFLIX</title>
-  <link rel="stylesheet" href="./style.css">
-</head>
-
-<body>
-  <div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <a href="https://www.icmc.usp.br/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <img src="https://avatars.githubusercontent.com/u/31321925?s=200&v=4" width="50px">
-      </a>
-    </header>
-  </div>
-
-  <div class="px-4 py-5 my-5 text-center">
-    <h1 class="display-5 fw-bold text-body-emphasis">USPFLIX</h1>
-    <div class="col-lg-6 mx-auto">
-      <p class="lead mb-4">Encontre seu filme favorito!</p>
-      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <input type="text" id="nomeFilme" placeholder="Nome do filme">
-        <button type="button" class="btn btn-outline-secondary btn-lg px-4" onclick="Pesquisar()">Pesquisar</button>
-      </div>
-    </div>
-  </div>
-
-  <div class="filmes"></div>
-
-   async function Pesquisar() {
+async function Pesquisar() {
   const inputValor = document.getElementById('pesquisar').value.trim();
   if (inputValor === '') {
     alert('Digite o nome do filme ou série para pesquisar.');
     return;
   }
 
-  const url = `https://imdb-top-100-movies.p.rapidapi.com/search?type=title&term=${encodeURIComponent(inputValor)}`;
+  const url = "https://imdb-top-100-movies.p.rapidapi.com/search?type=title&term=${encodeURIComponent(inputValor)}";
 
   const xhr = new XMLHttpRequest();
   xhr.withCredentials = true;
@@ -69,9 +38,5 @@
   xhr.setRequestHeader('x-rapidapi-key', 'b9715b912amsh8c346f5f7c302b3p1a80d4jsn47b14aaea364');
   xhr.setRequestHeader('x-rapidapi-host', 'imdb-top-100-movies.p.rapidapi.com');
 
-  xhr.send();
+xhr.send();
 }
-
-</body>
-
-</html>
